5844d9552ddcf1f69cbe9ef3d69c50e8babba6d2
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-11 15:17:56 +0100

    Fix: [brief description of your fix]

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin:[ "http://localhost:3000", "https://ecommerce-website-qr5tj727o-monisolas-projects-b08d7198.vercel.app/"],
   credentials: true               
 }));
 
