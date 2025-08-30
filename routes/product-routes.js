94cce7a88e7fa92124872565a1c7156020600429
Monisola102 <Oyewolemonisola102@gmail.com>
2025-08-30 17:45:42 +0100

    reviews controller updated

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -13,8 +13,8 @@ route.get("/:id", getSingleProduct)
 route.put("/:id", protect, adminOnly, upload.single("image"), updateProduct);
 route.delete("/:id", protect, adminOnly, deleteProduct);
 route.post("/:id/reviews", protect, addReview);
-route.put("/:id/reviews", protect,updateReview);
-route.get("/:id/reviews", getReviews); // fetch all reviews for a product
+route.put("/:id/reviews/:reviewId", protect,updateReview);
+route.get("/:id/reviews/", getReviews); // fetch all reviews for a product
 route.delete("/:id/reviews/:reviewId", protect, deleteReview);
 
 export default route;
